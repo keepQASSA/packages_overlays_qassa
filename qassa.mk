fd5128432c746859f4e0019a81e17e779a0946ad
Gnonymous7 <geraldfebrinzaghi7@gmail.com>
2020-08-03 22:04:00 +0700

    qassa: Add Gvisual Mod [3/3]
    - These are quashed commits from @Gnonymous7 as G-VisualMod creator, I intended to
    create good authorship culture.
    - PillGesture iOS as default
    
    https://github.com/Komodo-OS-Rom/vendor_komodo/commit/fb5ced7221ae9280c34fd4a8205d00f1df52c37e
    https://github.com/Komodo-OS-Rom/vendor_komodo/commit/b185e88c8a6abe69126eeea8301e52adb9ef8e33
    https://github.com/Komodo-OS-Rom/vendor_komodo/commit/5e0b0b376cf00e668460f2e55c2c8accaaabc48e
    https://github.com/Komodo-OS-Rom/vendor_komodo/commit/7941bca2059d592bea2247783ad1b0715a7177c6
    https://github.com/Komodo-OS-Rom/vendor_komodo/commit/ea4e2fcc3514686ecd49f42ff20d524b3e63639b
    
    Co-Authored-by: Kry9toN <dhimasbagusprayoga@gmail.com>
    Change-Id: Ifbe53c59581740581438f6400a0d1a0251757008
    Signed-off-by: LordShenron <shen.priyanshu@gmail.com>

---
+++ qassa.mk
@@ -0,0 +1,9 @@
+## Overlay makefile for building styles, themes and accents
+# GVM
+PRODUCT_PACKAGES += \
+    GVM-SBH-L \
+    GVM-SBH-M \
+    GVM-SBH-XL \
+    GVM-URM-M \
+    GVM-URM-L \
+    GVM-URM-R
