3121aabadd0d6c86920718ada717b31604e897c3
Advaith Bhat <advaithbhat9@gmail.com>
2021-04-26 08:57:48 +0000

    qassa: import more signal & wifi icons [2/2]
    - Thanks to sis, acherus, iconicicons & cool_modules_69 for these beautiful icons.
    
    Signed-off-by: Aston-Martinn <advaithbhat9@gmail.com>

--- qassa.mk
+++ qassa.mk
@@ -10,6 +10,8 @@ PRODUCT_PACKAGES += \
 
 # Statusbar Icons
 PRODUCT_PACKAGES += \
+    AcherusSignalOverlay \
+    DoraSignalOverlay \
     StrokeSignalOverlay \
     SneakySignalOverlay \
     XperiaSignalOverlay \
@@ -17,15 +19,47 @@ PRODUCT_PACKAGES += \
     WavySignalOverlay \
     RoundSignalOverlay \
     InsideSignalOverlay \
-    BarsSignalOverlay
+    BarsSignalOverlay \
+    AquariumSignalOverlay \
+    ButterflySignalOverlay \
+    DaunSignalOverlay \
+    DecSignalOverlay \
+    DeepSignalOverlay \
+    EqualSignalOverlay \
+    FanSignalOverlay \
+    HuaweiSignalOverlay \
+    RelSignalOverlay \
+    ScrollSignalOverlay \
+    SeaSignalOverlay \
+    StackSignalOverlay \
+    WannuiSignalOverlay \
+    WindowsSignalOverlay \
+    WingSignalOverlay \
+    CircleSignalOverlay \
+    IosSignalOverlay \
+    MiniSignalOverlay \
+    OdinSignalOverlay \
+    PillsSignalOverlay \
+    RomanSignalOverlay  \
+    GradiconSignalOverlay \
+    NothingDotSignalOverlay \
+    OutlineSignalOverlay \
+    PUISignalOverlay
 
 # Wi-Fi Icons
 PRODUCT_PACKAGES += \
+    DoraWiFiOverlay \
+    AcherusWiFiOverlay \
     StrokeWiFiOverlay \
     SneakyWiFiOverlay \
     XperiaWiFiOverlay \
     ZigZagWiFiOverlay \
     WavyWiFiOverlay \
+    WeedWiFiOverlay \
     RoundWiFiOverlay \
     InsideWiFiOverlay \
-    BarsWiFiOverlay
+    BarsWiFiOverlay \
+    GradiconWiFiOverlay \
+    OutlineWiFiOverlay \
+    NothingDotWiFiOverlay \
+    PavlovaWiFiOverlay
